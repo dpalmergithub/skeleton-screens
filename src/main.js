93b27f0ee865b6ada07c50a36b7c8ffb451cda5a
Daniel Palmer <dmpalmer212@gmail.com>
2020-05-13 11:53:27 -0400

    updated google analytics initialization in main.js

--- src/main.js
+++ src/main.js
@@ -11,6 +11,30 @@ import VueHighlightJS from 'vue-highlightjs'
 import VueClipboard from 'vue-clipboard2'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueMeta from 'vue-meta'
+
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxfPv9tJSBCnYYplmIHW-eREVlYt5ZaBE",
+  authDomain: "skeleton-screens.firebaseapp.com",
+  databaseURL: "https://skeleton-screens.firebaseio.com",
+  projectId: "skeleton-screens",
+  storageBucket: "skeleton-screens.appspot.com",
+  messagingSenderId: "418054163963",
+  appId: "1:418054163963:web:9368fb8cff28ea5d664f5e",
+  measurementId: "G-END4N5HEV3"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+Vue.prototype.$analytics = firebase.analytics();
+
+Vue.use(VueMeta)
 
 Vue.use(VueAxios, axios)
 
@@ -26,6 +50,7 @@ Vue.use(BootstrapVue)
 
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
 
 Vue.config.productionTip = false
 
